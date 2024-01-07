954dbcb2edfd8e2e60d82336a70fe48fb33deec0
Niltiwari7 <nt465638@gmail.com>
2024-01-07 12:29:14 +0530

    Created a middleware and function to handle the possible error

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,17 +1,19 @@
-
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs'
-export const signup = async (req, res) => {
+import { errorHandler } from "../utils/error.js";
+
+export const signup = async (req, res, next) => {
     const { username, email, password } = req.body
     console.log(req.body);
+    
+    const hashedPassword = bcryptjs.hashSync(password,12);
+   
+    const newUser = new User({username,email,password:hashedPassword});
 
-    const hasedPassword =  bcryptjs.hashSync(password,12);
-
-    const newUser = new User({ username, email, hasedPassword })
     try {
-        await newUser.save()
-        res.status(201).json("User Created sucessfully")
+        await newUser.save();
+        res.status(200).json('User created suceesfully');
     } catch (error) {
-        res.status(500).json(error.messsage);
+        next(error);
     }
 }
--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import mongoose from 'mongoose';
 import dotenv from 'dotenv'
+import bodyParser from 'body-parser';
 import userRouter from './routes/user.route.js'
 import authRouter from './routes/auth.route.js'
 
@@ -13,9 +14,20 @@ try {
     console.log(error);
 }
 const app = express()
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json())
-app.use('/api/user',userRouter)
-app.use('/api/auth',authRouter)
+app.use('/api/user', userRouter)
+app.use('/api/auth', authRouter)
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    res.status(statusCode).json({
+        success: false,
+        message,
+        statusCode,
+    });
+});
+
 app.listen(process.env.PORT, () => {
     console.log('Server is running on port 3000!!!');
 })
