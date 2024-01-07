f416c1048aa937810bf394d9aec2e26ba25b61a9
Niltiwari7 <nt465638@gmail.com>
2024-01-07 10:53:07 +0530

    Created a test api test

---
+++ api/controllers/user.controller.js
@@ -0,0 +1,6 @@
+export const test = (req,res)=>{
+    res.json({
+        message:"Hello I am nilesh Tiwari I am going to take super Dream placement"
+    })
+}
+
--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import mongoose from 'mongoose';
 import dotenv from 'dotenv'
-
+import userRouter from './routes/user.route.js'
 dotenv.config()
 try {
     mongoose.connect(process.env.MONGO).then(() => {
@@ -12,7 +12,7 @@ try {
 }
 const app = express()
 
-
+app.use('/api/user',userRouter)
 
 app.listen(process.env.PORT, () => {
     console.log('Server is running on port 3000!!!');
