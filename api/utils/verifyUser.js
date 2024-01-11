3d4fd66b0d55c6e54ef2fb85d2995abfce7d5047
Niltiwari7 <nt465638@gmail.com>
2024-01-11 21:06:42 +0530

    completed user update function , user delete system

--- api/utils/verifyUser.js
+++ api/utils/verifyUser.js
@@ -5,7 +5,7 @@ import { errorHandler } from './error.js';
 export const verifyToken = (req,res,next)=>{
     const token = req.cookies.access_token;
     if(!token){
-        return next(errorHandlerndler(401,"Unauthorized"));
+        return next(errorHandler(401,"Unauthorized"));
     }
     jwt.verify(token,process.env.JWT_SECRET,(err,user)=>{
        if(err){
