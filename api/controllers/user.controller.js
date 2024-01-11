4f117c6d81656879078120de8e1c6862c8940e76
Niltiwari7 <nt465638@gmail.com>
2024-01-11 18:59:55 +0530

    completed Update user functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,6 +1,33 @@
-export const test = (req,res)=>{
-    res.json({
-        message:"Hello I am nilesh Tiwari I am going to take super Dream placement"
-    })
-}
+import bcryptjs from 'bcryptjs';
+import User from '../models/user.model.js';
+import { errorHandler } from '../utils/error.js';
 
+export const updateUser = async (req, res, next) => {
+  try {
+    if (req.user.id !== req.params.id) {
+      return next(errorHandler(403, 'You can only update your account!'));
+    }
+
+    const updateUserFields = {
+      username: req.body.username,
+      email: req.body.email,
+      avatar: req.body.avatar,
+    };
+
+    if (req.body.password) {
+      updateUserFields.password = bcryptjs.hashSync(req.body.password, 10);
+    }
+
+    const updateUser = await User.findByIdAndUpdate(
+      req.params.id,
+      { $set: updateUserFields },
+      { new: true }
+    );
+
+    const { password, ...rest } = updateUser._doc;
+
+    res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv'
 import bodyParser from 'body-parser';
 import userRouter from './routes/user.route.js'
 import authRouter from './routes/auth.route.js'
+import cookieParser from 'cookie-parser';
 
 dotenv.config()
 try {
@@ -16,6 +17,7 @@ try {
 const app = express()
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json())
+app.use(cookieParser())
 app.use('/api/user', userRouter)
 app.use('/api/auth', authRouter)
 
