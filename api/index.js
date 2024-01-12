d422a56dd98e59ef22b22dd7ae5236bc9025b373
Niltiwari7 <nt465638@gmail.com>
2024-01-12 09:25:41 +0530

    completed Create Listing api functionality

--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ import bodyParser from 'body-parser';
 import userRouter from './routes/user.route.js'
 import authRouter from './routes/auth.route.js'
 import cookieParser from 'cookie-parser';
-
+import listingRouter from './routes/listing.route.js'
 dotenv.config()
 try {
     mongoose.connect(process.env.MONGO).then(() => {
@@ -20,6 +20,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use('/api/user', userRouter)
 app.use('/api/auth', authRouter)
+app.use('/api/listing',listingRouter)
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
