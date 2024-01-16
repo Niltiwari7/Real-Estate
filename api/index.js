352275a7e2d492700bdace6e8a36e345e7d3fadd
Niltiwari7 <nt465638@gmail.com>
2024-01-17 04:17:20 +0530

    updated index and vercel

--- api/index.js
+++ api/index.js
@@ -1,26 +1,29 @@
-import express from 'express'
+import express from 'express';
 import mongoose from 'mongoose';
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
 import bodyParser from 'body-parser';
-import userRouter from './routes/user.route.js'
-import authRouter from './routes/auth.route.js'
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 import cookieParser from 'cookie-parser';
-import listingRouter from './routes/listing.route.js'
-dotenv.config()
+import listingRouter from './routes/listing.route.js';
+
+dotenv.config();
+
 try {
-    mongoose.connect(process.env.MONGO).then(() => {
-        console.log(`MongoDb is connected on ${process.env.PORT}`);
-    })
+    await mongoose.connect(process.env.MONGO);
+    console.log(`MongoDb is connected on ${process.env.PORT}`);
 } catch (error) {
-    console.log(error);
+    console.error(error);
 }
-const app = express()
+
+const app = express();
+
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.json())
-app.use(cookieParser())
-app.use('/api/user', userRouter)
-app.use('/api/auth', authRouter)
-app.use('/api/listing',listingRouter)
+app.use(express.json());
+app.use(cookieParser());
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/listing', listingRouter);
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
@@ -32,6 +35,10 @@ app.use((err, req, res, next) => {
     });
 });
 
-app.listen(process.env.PORT, () => {
-    console.log('Server is running on port 3000!!!');
-})
+app.listen(process.env.PORT)
+    .then(() => {
+        console.log(`Server is running on port ${process.env.PORT}`);
+    })
+    .catch((error) => {
+        console.error(`Error starting server: ${error}`);
+    });
