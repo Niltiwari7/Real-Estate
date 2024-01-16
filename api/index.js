c8b6d1121885b943027d946870c2089426591bfb
Niltiwari7 <nt465638@gmail.com>
2024-01-17 04:25:19 +0530

    updated index.js

--- api/index.js
+++ api/index.js
@@ -1,37 +1,43 @@
-import express from 'express'
+import express from 'express';
 import mongoose from 'mongoose';
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
 import bodyParser from 'body-parser';
-import userRouter from './routes/user.route.js'
-import authRouter from './routes/auth.route.js'
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 import cookieParser from 'cookie-parser';
-import listingRouter from './routes/listing.route.js'
-dotenv.config()
-try {
-    mongoose.connect(process.env.MONGO).then(() => {
+import listingRouter from './routes/listing.route.js';
+
+dotenv.config();
+
+const startServer = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO);
         console.log(`MongoDb is connected on ${process.env.PORT}`);
-    })
-} catch (error) {
-    console.log(error);
-}
-const app = express()
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.json())
-app.use(cookieParser())
-app.use('/api/user', userRouter)
-app.use('/api/auth', authRouter)
-app.use('/api/listing',listingRouter)
+        
+        const app = express();
+        app.use(bodyParser.urlencoded({ extended: true }));
+        app.use(express.json());
+        app.use(cookieParser());
+        app.use('/api/user', userRouter);
+        app.use('/api/auth', authRouter);
+        app.use('/api/listing', listingRouter);
 
-app.use((err, req, res, next) => {
-    const statusCode = err.statusCode || 500;
-    const message = err.message || 'Internal Server Error';
-    res.status(statusCode).json({
-        success: false,
-        message,
-        statusCode,
-    });
-});
+        app.use((err, req, res, next) => {
+            const statusCode = err.statusCode || 500;
+            const message = err.message || 'Internal Server Error';
+            res.status(statusCode).json({
+                success: false,
+                message,
+                statusCode,
+            });
+        });
 
-app.listen(process.env.PORT, () => {
-    console.log('Server is running on port 3000!!!');
-})
+        app.listen(process.env.PORT, () => {
+            console.log(`Server is running on port ${process.env.PORT}`);
+        });
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+startServer();
