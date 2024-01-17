94afb54a81b09116addb74aa56b69e8616afdcfc
Niltiwari7 <nt465638@gmail.com>
2024-01-17 06:20:48 +0530

    updated files

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 import cookieParser from 'cookie-parser';
 import listingRouter from './routes/listing.route.js';
+import path from 'path'
 
 dotenv.config();
 
@@ -13,7 +14,8 @@ const startServer = async () => {
     try {
         await mongoose.connect(process.env.MONGO);
         console.log(`MongoDb is connected on ${process.env.PORT}`);
-        
+        const __dirname = path.resolve();
+
         const app = express();
         app.use(bodyParser.urlencoded({ extended: true }));
         app.use(express.json());
@@ -21,7 +23,10 @@ const startServer = async () => {
         app.use('/api/user', userRouter);
         app.use('/api/auth', authRouter);
         app.use('/api/listing', listingRouter);
-
+        app.use(express.static(path.join(__dirname+'/client/dist')));
+        app.get("*",(req,res)=>{
+          res.sendFile(path.join(__dirname,'client','dist','index.html'))  ;
+        })
         app.use((err, req, res, next) => {
             const statusCode = err.statusCode || 500;
             const message = err.message || 'Internal Server Error';
