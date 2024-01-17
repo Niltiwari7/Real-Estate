02fe5bfb74f8e8d50eb6a5e066ee92b03847e34a
Niltiwari7 <nt465638@gmail.com>
2024-01-17 11:15:36 +0530

    updated Index.jx file

--- api/index.js
+++ api/index.js
@@ -1,48 +1,51 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import bodyParser from 'body-parser';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
-import cookieParser from 'cookie-parser';
 import listingRouter from './routes/listing.route.js';
-import path from 'path'
-
+import cookieParser from 'cookie-parser';
+import path from 'path';
 dotenv.config();
 
-const startServer = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO);
-        console.log(`MongoDb is connected on ${process.env.PORT}`);
-        const __dirname = path.resolve();
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('Connected to MongoDB!');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-        const app = express();
-        app.use(bodyParser.urlencoded({ extended: true }));
-        app.use(express.json());
-        app.use(cookieParser());
-        app.use('/api/user', userRouter);
-        app.use('/api/auth', authRouter);
-        app.use('/api/listing', listingRouter);
-        app.use(express.static(path.join(__dirname+'/client/dist')));
-        app.get("*",(req,res)=>{
-          res.sendFile(path.join(__dirname,'client','dist','index.html'))  ;
-        })
-        app.use((err, req, res, next) => {
-            const statusCode = err.statusCode || 500;
-            const message = err.message || 'Internal Server Error';
-            res.status(statusCode).json({
-                success: false,
-                message,
-                statusCode,
-            });
-        });
+  const __dirname = path.resolve();
 
-        app.listen(process.env.PORT, () => {
-            console.log(`Server is running on port ${process.env.PORT}`);
-        });
-    } catch (error) {
-        console.error(error);
-    }
-};
+const app = express();
 
-startServer();
+app.use(express.json());
+
+app.use(cookieParser());
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000!');
+});
+
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/listing', listingRouter);
+
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
